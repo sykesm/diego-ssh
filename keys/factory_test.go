5131125c992c8fe73abf452778dd3604d000b8ec
Matthew Sykes <matthew.sykes@gmail.com>
2015-12-28 17:42:19 -0500

    wip - key factory pool

---
+++ keys/factory_test.go
@@ -0,0 +1,15 @@
+// +build ignore
+package keys_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Factory", func() {
+
+	BeforeEach(func() {
+		Expect(true).To(BeTrue())
+	})
+
+})
